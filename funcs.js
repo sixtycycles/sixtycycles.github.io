02e4004a7de8fa54f3c13adf4eb4e27450a8f3dd
Rod <sixtycycles@gmail.com>
2017-03-31 21:11:50 -0400

    up!

---
+++ funcs.js
@@ -0,0 +1,8 @@
+/**
+ * Created by rod on 3/31/17.
+ */
+function about(){
+    document.getElementById('about').style.visibility = visible()
+    document.getElementById('projects').style.visibility = "hidden";
+    document.getElementById('fun').style.visibility = "hidden";
+}
