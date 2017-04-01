38b376d2086916ece04b7fe6c64fe066e6ef65ec
Rod <sixtycycles@gmail.com>
2017-04-01 10:09:48 -0400

    typos
    yeah autocomplete ruining my life.

--- funcs.js
+++ funcs.js
@@ -1,8 +1,25 @@
 /**
  * Created by rod on 3/31/17.
  */
+
+function setup(){
+document.getElementById('about').style.visibility="hidden"
+    document.getElementById('projects').style.visibility="hidden"
+    document.getElementById('fun').style.visibility="hidden"
+}
+
 function about(){
-    document.getElementById('about').style.visibility = visible()
+    document.getElementById('about').style.visibility = "visible";
     document.getElementById('projects').style.visibility = "hidden";
     document.getElementById('fun').style.visibility = "hidden";
+}
+function projects(){
+    document.getElementById('about').style.visibility = "hidden";
+    document.getElementById('projects').style.visibility = "visible";
+    document.getElementById('fun').style.visibility = "hidden";
+}
+function fun(){
+    document.getElementById('about').style.visibility = "hidden";
+    document.getElementById('projects').style.visibility = "hidden";
+    document.getElementById('fun').style.visibility = "visible";
 }
